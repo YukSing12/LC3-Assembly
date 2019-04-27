041442e2ef252bb7ba71da7a4588896528705f16
YukSing12 <502322584@qq.com>
2019-04-27 15:20:47 +0000

    lab8

---
+++ lab8/input.asm
@@ -0,0 +1,10 @@
+        .ORIG x5000
+        .FILL x0048
+        .FILL x0065
+        .FILL x006c
+        .FILL x006c
+        .FILL x006f
+        .FILL x002e
+        .FILL x0000
+        .END
+
---
+++ lab8/onetest.asm
@@ -0,0 +1,11 @@
+; This is the string you are going to print on monitor
+
+      .ORIG x5000
+INPUT .FILL x0048  ; 'H'
+      .FILL x0065  ; 'e'
+      .FILL x006C  ; 'l'
+      .FILL x006C  ; 'l'
+      .FILL x006F  ; 'o'
+      .FILL x002E  ; '.'
+      .FILL x0000  ; null
+      .END
